8418fb09214c2b2508ef47cbdaceb993141f98d8
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-11-09 10:31:26 +0600

    Customize card color based on priority

--- app/notes/_components/NoteCard.tsx
+++ app/notes/_components/NoteCard.tsx
@@ -19,10 +19,24 @@ const NoteCard = ({ note }: Props) => {
     wasUpdated ? note.updatedAt : note.createdAt
   ).toDateString();
 
+  const className = (priority: string) => {
+    return `${
+      priority === "LOW"
+        ? "blue-500"
+        : priority === "HIGH"
+        ? "rose-500"
+        : "orange-500"
+    }`;
+  };
+
+  const color = className(note.priority);
+
   return (
     <div className="relative h-fit cursor-pointer">
-      <span className="absolute left-0 top-0 ml-1 mt-1 h-full w-full rounded-lg bg-green-500 "></span>
-      <Card className="relative border-green-500">
+      <span
+        className={`absolute left-0 top-0 ml-1 mt-1 h-full w-full rounded-lg bg-${color}`}
+      ></span>
+      <Card className={`relative border-${color}`}>
         <CardHeader>
           <CardTitle className="line-clamp-1">{note.title}</CardTitle>
           <CardDescription>{createdUpdateAtTimestamp}</CardDescription>
