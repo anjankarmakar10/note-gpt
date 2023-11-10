f261863163ab421dadc56b1f949f3756311575af
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-11-10 10:00:55 +0600

    Set up openai and pinecone

--- app/notes/Navbar.tsx
+++ app/notes/Navbar.tsx
@@ -14,7 +14,7 @@ import { UserButton } from "@clerk/nextjs";
 import ThemeToggle from "@/components/ThemeToggle";
 const Navbar = () => {
   return (
-    <header className="sticky top-0 z-50  bg-white shadow dark:bg-slate-800">
+    <header className="sticky top-0 z-50  bg-white shadow dark:bg-slate-950">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2">
         <Link href="/notes" className="flex items-center gap-1 font-bold">
           <Image src={logo} alt="Logo" width={40} height={40} />
